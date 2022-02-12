7446f09e93c552dd3f24d4f129b9b5bb47f28cad
srtaseen <srtaseen@gmail.com>
2022-02-12 20:25:50 +0600

    port no

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('welcome to the home page')
 })
 
-app.listen(3000, () => {
+const port = process.env.port || 3000
+app.listen(port, () => {
   console.log('listening on port 3000')
 })
